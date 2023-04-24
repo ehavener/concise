02ef4c136f9e7852b81914d61b718baa90f617df
Emerson Havener <emerson@nevada.unr.edu>
2023-04-24 09:58:02 -0700

    Connect signUp to spring endpoint

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,3 +1,14 @@
+import styles from "@/styles/login.module.css";
+
 export default function Login() {
-    return (<div>Hello!</div>)
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <p style={{textAlign: "center"}}>Login</p>
+                <input type="email" name="email" placeholder="Email"/>
+                <input type="password" name="password" placeholder="Password"/>
+                <button type="button">Submit</button>
+            </div>
+        </div>
+    )
 }
--- frontend/pages/signUp.tsx
+++ frontend/pages/signUp.tsx
@@ -1,17 +1,64 @@
 import styles from '../styles/signUp.module.css';
+import {Component} from "react";
 
-export default function SignUp() {
+export default class SignUp extends Component<any, any>{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
 
+    handleEmailChange(event: any) {
+        this.setState({
+            email: event.target.value
+        });
+    }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <p style={{textAlign: "center"}}>Sign Up</p>
-                <input type="email" name="email" placeholder="Email"/>
-                <input type="password" name="password" placeholder="Password"/>
-                <button type="button">Submit</button>
+    handlePasswordChange(event: any) {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handleSubmit(event: any) {
+        const url: string  = "http://127.0.0.1:8080/api/v1/users/sign-up"
+        fetch(url, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+            .then((response) => response.json())
+            .then(data => {
+                this.setState({ ...this.state, bearer: data['access_token'] })
+                localStorage.setItem("ot_access_token", data['access_token']);
+                // window.location.replace(`${window.location.origin}/`);
+            })
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <p style={{textAlign: "center"}}>Sign Up</p>
+                    <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange}/>
+                    <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                    <button type="button" onClick={this.handleSubmit}>Submit</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
