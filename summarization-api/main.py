b560126ea6295c2f1f8ba8bb9a41462ae32f89fd
Emerson Havener <emerson@nevada.unr.edu>
2023-05-19 17:27:34 -0700

    - add .dockerignore so that .env does not get included in built docker images - add notes to dockerfile - implement summarize endpoint for lmsys/fastchat-t5-3b-v1.0 model - update requirements.txt for lmsys/fastchat-t5-3b-v1.0

--- summarization-api/main.py
+++ summarization-api/main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI, Depends, HTTPException, status
-
 from fastapi.security import APIKeyHeader
-from dotenv import load_dotenv
 import os
 
-import textwrap
+from pydantic import BaseModel
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-from pydantic.main import BaseModel
-
-load_dotenv()  # take environment variables from .env.
 API_KEY = os.getenv("API_KEY")  # Get API Key from environment variable
 API_KEY_NAME = "X-API-Key"
 
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 app = FastAPI()
+
 
 async def get_api_key(api_key_header: str = Depends(api_key_header)):
     if api_key_header == API_KEY:
@@ -25,11 +23,36 @@ async def get_api_key(api_key_header: str = Depends(api_key_header)):
             detail="Invalid API Key",
         )
 
+
 class Text(BaseModel):
     text: str
 
+
+# Check if GPU is available and if not, use CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the model and tokenizer
+model_name = "lmsys/fastchat-t5-3b-v1.0"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+# Move model to the device
+model = model.to(device)
+
+
 @app.post("/summarize/", dependencies=[Depends(get_api_key)])
 async def summarize(text: Text):
-    summary = textwrap.shorten(text.text, width=100)  # This just shortens the text to 100 characters
-    return {"summary": summary}
+    # Encode input text
+    inputs = tokenizer.encode(text.text, return_tensors="pt")
 
+    # Move inputs to the device
+    inputs = inputs.to(device)
+
+    # Generate output
+    outputs = model.generate(inputs, max_length=1024, temperature=0.7, top_p=1.0, do_sample=True)
+
+    # Decode the output
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    summary = summary.replace("  ", " ")
+
+    return {"summary": summary}
