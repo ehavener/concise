0e0e0fd9975473e04d23dcb3e2e261f4bd89be1b
Emerson Havener <emerson@nevada.unr.edu>
2023-05-22 22:44:28 -0700

    - Decrease temperature for more conservative summaries. Decrease top p slightly to prevent looping hallucinations.

--- summarization-api/main.py
+++ summarization-api/main.py
@@ -39,15 +39,13 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 # Move model to the device
 model = model.to(device)
 
-# TODO: Recursive summarization for prompts that exceed maximum length supported by 24GB GPU RAM.
-# TODO: Implement Task Queues using Celery
-
 @app.post("/summarize/", dependencies=[Depends(get_api_key)])
 async def summarize(body: RequestBodyModel):
     # Encode input text
     inputs = tokenizer.encode(body.text, return_tensors="pt")
 
     # Calculate the maximum length for the generate method. Constraint: input_length + generation length <= 2048 tokens
+    # TODO: Chunk summaries per 1500-ish tokens. Determine appropriate summary lengths. Recurse if necessary.
     if inputs.shape[1] > 1500:
         inputs = inputs[:, :1500]
 
@@ -55,7 +53,7 @@ async def summarize(body: RequestBodyModel):
     inputs = inputs.to(device)
 
     # Generate output. 500 Tokens is 20 sentences.
-    outputs = model.generate(inputs, max_length=500, temperature=0.7, top_p=1.0, do_sample=True)
+    outputs = model.generate(inputs, max_length=500, temperature=0.2, top_p=0.9, do_sample=True)
 
     # Decode the output
     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
