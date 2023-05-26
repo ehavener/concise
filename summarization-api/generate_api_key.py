9b4ebae45dcba89b0b87a7a30ce7626fb34ef5f1
Emerson Havener <emerson@nevada.unr.edu>
2023-05-25 21:30:58 -0700

    - refactor summarization-api to use RabbitMQ instead of REST - prevent cut-off sentences from being included in summarization output - reduce summary length from 350 to 250 tokens

--- summarization-api/generate_api_key.py
+++
@@ -1,8 +0,0 @@
-import os
-import base64
-
-def generate_api_key(length=32):
-    key = os.urandom(length)
-    return base64.urlsafe_b64encode(key).rstrip(b'=').decode('utf-8')
-
-print(generate_api_key())
--- summarization-api/main.py
+++ summarization-api/main.py
@@ -1,32 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import APIKeyHeader
-import os
+from datetime import datetime
 
-from pydantic import BaseModel
+import pika
+import json
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-API_KEY = os.getenv("API_KEY")  # Get API Key from environment variable
-API_KEY_NAME = "X-API-Key"
-
-api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
-
-app = FastAPI()
-
-
-async def get_api_key(api_key_header: str = Depends(api_key_header)):
-    if api_key_header == API_KEY:
-        return api_key_header
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid API Key",
-        )
-
-
-class RequestBodyModel(BaseModel):
-    text: str
-
 
 # Check if GPU is available and if not, use CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,31 +16,81 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 # Move model to the device
 model = model.to(device)
 
-# TODO: Test a model with a larger context window for better summaries of >10 minute videos. MPT or RWKV.
-# TODO: Improve summary quality.
-# TODO: Improve per-token inference speed.
-@app.post("/summarize/", dependencies=[Depends(get_api_key)])
-async def summarize(body: RequestBodyModel):
-    # Encode input text
-    inputs = tokenizer.encode(body.text, return_tensors="pt")
+# TODO: Test a model with a larger context window for better summaries of >10 minute videos. MPT | RWKV | Guanaco.
+# TODO: Improve summary quality. Improve per-token inference speed.
+def summarize(text):
+    # add summarization prompt to text
+    text = "Summarize the following video transcript text into a 5-10 sentence paragraph.\n\n" + text + "TL;DR: "
 
-    # Calculate the maximum length for the generate method. Constraint: input_length + generation length <= 2048 tokens
-    # TODO: Chunk summaries per 1500-ish tokens. Determine appropriate summary lengths. Improve prompting (send previous summary?).
-    #  Make video summaries 2-4 sentences per 10 min of video. 10 min is roughly 1500 tokens.
-    #  Make chapter summaries 1-3 sentences.
-    if inputs.shape[1] > 1650:
-        inputs = inputs[:, :1650]
+    max_length = 1750
+
+    # Encode input text
+    tokenized_text = tokenizer.tokenize(text, return_tensors="pt")
+
+    # Ensure no sentence is cut off at the start
+    if not tokenized_text[0].isupper() and not tokenized_text[0].isdigit():
+        for i in range(len(tokenized_text)):
+            if tokenized_text[i] in {'.', '!', '?'}:
+                tokenized_text = tokenized_text[i+1:]
+                break
+
+    # Ensure no sentence is cut off at the end
+    encoded_text = []
+    sentence = []
+    for token in tokenized_text:
+        if len(encoded_text) + len(sentence) + 1 > max_length:
+            break
+        elif token in {'.', '!', '?'}:
+            sentence.append(token)
+            encoded_text.extend(sentence)
+            sentence = []
+        else:
+            sentence.append(token)
+
+    return tokenizer.convert_tokens_to_ids(encoded_text)
+
+    inputs = tokenizer.convert_tokens_to_ids(encoded_text)
+
+    print(len(inputs))
 
     # Move inputs to the device
     inputs = inputs.to(device)
 
     # Generate output. 250 Tokens is 10 sentences.
     # TODO: Evaluate results using 8-bit precision.
-    outputs = model.generate(inputs, max_length=350, temperature=0.2, top_p=0.9, do_sample=True)
+    outputs = model.generate(inputs, max_length=250, temperature=0.2, top_p=0.9, do_sample=True)
 
     # Decode the output
     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
     summary = summary.replace("  ", " ")
     summary = summary.replace("<pad> ", "")  # Remove leading pad token
+    print("completed generation for: ", len(inputs))
+    return summary
 
-    return {"summary": summary}
+def callback(ch, method, properties, body):
+    print("Received request at ", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print(ch, method, properties, body)
+    message = json.loads(body)
+    # print(message)
+    summary = summarize(message["transcript"])
+    json_message = json.dumps({
+        "videoId": message["videoId"],
+        "chapterId" : message["chapterId"],
+        "summary": summary})
+    channel.basic_publish(exchange='', routing_key='summarized', body=json_message)
+
+
+# RabbitMQ connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='toSummarize')
+
+# Tell RabbitMQ that this particular function should receive messages from our toSummarize queue
+channel.basic_consume(queue='toSummarize', on_message_callback=callback, auto_ack=True)
+
+print('Waiting for messages. To exit press CTRL+C')
+
+# Start a never-ending loop that waits for data and runs callbacks whenever necessary
+channel.start_consuming()
